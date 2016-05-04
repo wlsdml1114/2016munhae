342871c8f62fddcc466363f6741f3c70b584e037
jini <wlsdml1114@gmail.com>
2016-05-05 00:19:08 +0900

    Sp2

---
+++ sp2/my_assembler.c
@@ -0,0 +1,497 @@
+/*
+* 화일명 : my_assembler.c
+* 설  명 : 이 프로그램은 SIC/XE 머신을 위한 간단한 Assembler 프로그램의 메인루틴으로,
+* 입력된 파일의 코드 중, 명령어에 해당하는 OPCODE를 찾아 출력한다.
+*
+*/
+
+/*
+*
+* 프로그램의 헤더를 정의한다.
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+* my_assembler 프로그램의 의존적인 데이터들이 정의된 곳이다.
+*/
+
+#include "my_assembler.h"
+
+/* -----------------------------------------------------------------------------------
+* 설명 : 사용자로 부터 어셈블리 파일을 받아서 명령어의 OPCODE를 찾아 출력한다.
+* 매계 : 실행 파일, 어셈블리 파일
+* 반환 : 성공 = 0, 실패 = < 0
+* 주의 : 현재 어셈블리 프로그램의 리스트 파일을 생성하는 루틴은 만들지 않았다.
+*		   또한 중간파일을 생성하지 않는다.
+* -----------------------------------------------------------------------------------
+*/
+
+
+int main(int args, char *arg[])
+{
+	if (init_my_assembler()< 0)
+	{
+		printf("init_my_assembler: 프로그램 초기화에 실패 했습니다.\n");
+		return -1;
+	}
+
+	if (assem_pass1() < 0) {
+		printf("assem_pass1: 패스1 과정에서 실패하였습니다.  \n");
+
+		return -1;
+	}
+	if (assem_pass2() < 0) {
+		printf(" assem_pass2: 패스2 과정에서 실패하였습니다.  \n");
+		return -1;
+	}
+//	printf("%X\t%X\t%X",locctr[0],locctr[1],locctr[2]);
+//	int i,j;
+//	for(i=0;i<sectionIndex;++i){
+//		for(j=0;j<symbol_line[i];++j){
+//			printf("%04hhX\t%s\n",sym_table[i][j].addr,sym_table[i][j].symbol);
+//		}
+//		printf("\n");
+//	}
+
+	//make_output("output") ;
+}
+/* -----------------------------------------------------------------------------------
+* 설명 : 프로그램 초기화를 위한 자료구조 생성 및 파일을 읽는 함수이다.
+* 매계 : 없음
+* 반환 : 정상종료 = 0 , 에러 발생 = -1
+* 주의 : 각각의 명령어 테이블을 내부에 선언하지 않고 관리를 용이하게 하기
+*		   위해서 파일 단위로 관리하여 프로그램 초기화를 통해 정보를 읽어 올 수 있도록
+*		   구현하였다.
+* -----------------------------------------------------------------------------------
+*/
+
+int init_my_assembler(void)
+{
+	int result;
+
+	if ((result = init_inst_file("inst.data")) < 0)
+		return -1;
+	if ((result = init_input_file("input.txt")) < 0)
+		return -1;
+	return result;
+}
+
+/* -----------------------------------------------------------------------------------
+* 설명 : 어셈블리 코드를 위한 패스1과정을 수행하는 함수이다.
+*		   패스1에서는..
+*		   1. 프로그램 소스를 스캔하여 해당하는 토큰단위로 분리하여 프로그램 라인별 토큰
+*		   테이블을 생성한다.
+*
+* 매계 : 없음
+* 반환 : 정상 종료 = 0 , 에러 = < 0
+* 주의 : 현재 초기 버전에서는 에러에 대한 검사를 하지 않고 넘어간 상태이다.
+*	  따라서 에러에 대한 검사 루틴을 추가해야 한다.
+*
+* -----------------------------------------------------------------------------------
+*/
+
+static int assem_pass1(void)
+{
+	int i=0;
+	for(;i<line_num;++i){
+		int result;
+		if ((result = token_parsing(i)) < 0){//output is less then 0 means this sentence start with .
+			continue;
+		}
+//		printf("%d\n",search_opcode(token_table[token_line].operator_));
+		if( strcmp(token_table[token_line].operator_, "START") == 0){
+			locctr[sectionIndex] = (int)strtol(token_table[token_line].operand[0],NULL,16);
+			sections[sectionIndex] = (char *)malloc(strlen(token_table[token_line].label));
+			strcpy(sections[sectionIndex++], token_table[token_line].label);
+//			printf("%04hhX  %s\n",locctr[sectionIndex-1],sections[sectionIndex-1]);
+			token_table[token_line].addr= locctr[sectionIndex-1];
+			token_table[token_line].length = 0;
+		}
+		else if( strcmp(token_table[token_line].operator_, "CSECT") == 0){
+			sections[sectionIndex] = (char *)malloc(strlen(token_table[token_line].label));
+			strcpy(sections[sectionIndex++], token_table[token_line].label);
+			locctr[sectionIndex-1]=0;
+			literal_number[sectionIndex-1]=0;
+			symbol_line[sectionIndex-1]=0;
+//			printf("%04hhX  %s\n",locctr[sectionIndex-1],sections[sectionIndex-1]);
+			token_table[token_line].addr= locctr[sectionIndex-1];
+			token_table[token_line].length = 0;
+		}
+		else if( strcmp(token_table[token_line].operator_, "EXTDEF") == 0){
+			token_table[token_line].addr= 0;
+			token_table[token_line].length = 0;
+		}
+		else if( strcmp(token_table[token_line].operator_, "EXTREF") == 0){
+			token_table[token_line].addr= locctr[sectionIndex-1];
+			token_table[token_line].length = 0;
+			char *token;
+			const char s[2] = ",";
+			char buffer[100];
+			strcpy(buffer,token_table[token_line].operand[0]);
+			token = strtok(buffer,s);
+			int i=0;
+			do{
+//				sym_table[sectionIndex-1][symbol_line].symbol = (char *)malloc(sizeof(strlen(token)));
+				token_table[token_line].operand[i] = malloc(sizeof(token));
+				strcpy(token_table[token_line].operand[i++],token);
+				strcpy(sym_table[sectionIndex-1][symbol_line[sectionIndex-1]].symbol,token);
+				sym_table[sectionIndex-1][symbol_line[sectionIndex-1]++].addr = 0;
+				token = strtok(NULL,s);
+			}
+			while(token!=NULL);
+		}
+		else if( strcmp(token_table[token_line].operator_, "LTORG" ) == 0 ){
+			token_table[token_line].addr= locctr[sectionIndex-1];
+			if(liter[sectionIndex-1][literal_number[sectionIndex-1]-1].literal[1]=='C'){
+				locctr[sectionIndex-1]+=strlen(liter[sectionIndex-1][literal_number[sectionIndex-1]-1].literal)-4;
+			}else{
+				locctr[sectionIndex-1]+=(strlen(liter[sectionIndex-1][literal_number[sectionIndex-1]-1].literal)-4)*0.5;
+			}
+		}
+
+		else if( strcmp(token_table[token_line].operator_, "END" ) == 0){
+			token_table[token_line].addr= locctr[sectionIndex-1];
+			locctr[sectionIndex-1]++;
+		}
+
+		else{
+			int indexOfInst;
+			token_table[token_line].addr = locctr[sectionIndex-1];
+			if( strcmp(token_table[token_line].label, "") != 0 ){
+				int j=0,status=0;
+				for(;j<symbol_line[sectionIndex-1];++j){
+					if(strcmp(sym_table[sectionIndex-1][j].symbol,token_table[token_line].label)==0){
+						status++;
+					}
+				}
+				if(status==0){
+//					sym_table[sectionIndex-1][symbol_line].symbol = (char *)malloc(sizeof(strlen(token_table[token_line].label)));
+					strcpy(sym_table[sectionIndex-1][symbol_line[sectionIndex-1]].symbol,token_table[token_line].label);
+					sym_table[sectionIndex-1][symbol_line[sectionIndex-1]++].addr = locctr[sectionIndex-1];
+				}
+			}
+			// label 중복이면 에러, 아니면 심볼테이블에 추가
+			if ( (indexOfInst = search_opcode(token_table[token_line].operator_)) > -1 ){
+				if( token_table[token_line].operator_[0] == '+' ){
+					locctr[sectionIndex-1]+=4;
+				}
+				else {
+					locctr[sectionIndex-1]+=inst[indexOfInst].format;
+				}
+			}
+			// opcode 를 인스트럭션 테이블에서 찾음
+			//존재하면 일반 2형씩 또는 3형식에 따라 더함
+			// + 가 있을 경우 4형식이므로 +4
+			else if( strcmp(token_table[token_line].operator_, "WORD" ) == 0){
+				locctr[sectionIndex-1]+=3;
+			}
+			// WORD 이면 +3
+			else if( strcmp(token_table[token_line].operator_, "RESW" ) == 0){
+				locctr[sectionIndex-1]+= atoi(token_table[token_line].operand[0])*3;
+			}
+			// RESW 이면 + (3 * 갯수)
+			else if( strcmp(token_table[token_line].operator_, "RESB" ) == 0){
+
+				locctr[sectionIndex-1]+= atoi(token_table[token_line].operand[0]);
+			}
+			// RESB 이면 + (1 * 갯수)
+			else if( strcmp(token_table[token_line].operator_, "BYTE" ) == 0){
+				if(token_table[token_line].operand[0][0]=='X'){
+					locctr[sectionIndex-1] += (strlen(token_table[token_line].operand[0])-4)*0.5;
+					locctr[sectionIndex-1]++;
+				}else{
+					locctr[sectionIndex-1] += (strlen(token_table[token_line].operand[0])-4);
+				}
+			}
+			// 캐릭터형이면 + (1 * 문자열갯수)
+			// 16진수이면 + (0.5 * 숫자갯수)
+			else if(strcmp(token_table[token_line].operator_, "EQU")==0 ){
+				token_table[token_line].addr = locctr[sectionIndex-1];
+				if(strcmp(token_table[token_line].label, "MAXLEN")==0){
+
+					token_table[token_line].addr = 4096;
+				}
+
+
+			}
+			else{
+				return -1;
+			}
+			if(token_table[token_line].operand[0][0]=='='){
+				liter[sectionIndex-1][literal_number[sectionIndex-1]].literal = (char *)malloc(sizeof(strlen(token_table[token_line].operand[0])));
+				strcpy(liter[sectionIndex-1][literal_number[sectionIndex-1]++].literal,token_table[token_line].operand[0]);
+			}
+		}
+//		printf("%04X\t%s\t%s\t%s\t%s\n",token_table[token_line].addr,token_table[token_line].label,token_table[token_line].operator_,token_table[token_line].operand[0],token_table[token_line].operand[1]);
+		token_line++;
+	}
+	return 0;
+}
+
+/* -----------------------------------------------------------------------------------
+* 설명 : 어셈블리 코드를 기계어 코드로 바꾸기 위한 패스2 과정을 수행하는 함수이다.
+*		   패스 2에서는 프로그램을 기계어로 바꾸는 작업은 라인 단위로 수행된다.
+*		   다음과 같은 작업이 수행되어 진다.
+*		   1. 실제로 해당 어셈블리 명령어를 기계어로 바꾸는 작업을 수행한다.
+* 매계 : 없음
+* 반환 : 정상종료 = 0, 에러발생 = < 0
+* 주의 :
+* -----------------------------------------------------------------------------------
+*/
+
+static int assem_pass2(void)
+{
+	int i=0,indexOfInst,section=0;
+	for(;i<token_line;++i){
+		printf("%04X\t%s\t%s",token_table[i].addr,token_table[i].label,token_table[i].operator_);
+		if(token_table[i].operand[0]!=NULL){
+			printf("\t%s",token_table[i].operand[0]);
+		}
+		if(token_table[i].operand[1]!=NULL){
+			printf(",%s",token_table[i].operand[1]);
+		}
+		if( strcmp(token_table[i].operator_, "CSECT") == 0){
+			section++;
+		}
+		char *buffer = (char *)malloc(token_table[i].length);
+		char reg[6];
+		if(token_table[i].length==2){
+			indexOfInst = search_opcode(token_table[i].operator_);
+
+		}
+
+		else if((indexOfInst = search_opcode(token_table[i].operator_)) > -1){
+			if(token_table[i].operand[0][0]=='#'){//nixbpe
+				reg[0] = 0; reg[1] = 1; reg[2] = 0;
+				reg[3] = 0; reg[4] = 0; reg[5] = 0;
+			}else if(token_table[i].operator_[0]=='+'){
+				reg[0] = 1; reg[1] = 1; reg[2] = 0;
+				reg[3] = 0; reg[4] = 0; reg[5] = 1;
+			}else if(token_table[i].operand[0][0]=='@'){
+				reg[0] = 1; reg[1] = 0; reg[2] = 0;
+				reg[3] = 0; reg[4] = 1; reg[5] = 0;
+			}else{
+				reg[0] = 1; reg[1] = 1; reg[2] = 0;
+				reg[3] = 0; reg[4] = 1; reg[5] = 0;
+			}
+
+
+
+		}
+
+
+		else if(strcmp(token_table[i].operator_,"END")==0||
+				strcmp(token_table[i].operator_,"BYTE")==0||
+				strcmp(token_table[i].operator_,"WORD")==0||
+				strcmp(token_table[i].operator_,"LTORG")==0){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}
+
+		printf("\n");
+	}
+	make_objectcode("output.txt");
+	return 0;
+}
+/* -----------------------------------------------------------------------------------
+* 설명 : 머신을 위한 기계 코드목록 파일을 읽어 기계어 목록 테이블(inst_table)을
+*        생성하는 함수이다.
+* 매계 : 기계어 목록 파일
+* 반환 : 정상종료 = 0 , 에러 < 0
+* 주의 : 기계어 목록파일 형식은 다음과 같다.
+*
+*	===============================================================================
+*		   | 이름 | 형식 | 기계어 코드 | 오퍼랜드의 갯수 | NULL|
+*	===============================================================================
+*
+* -----------------------------------------------------------------------------------
+*/
+
+int init_inst_file(char *inst_file)
+{
+	FILE *in_fp;
+	int i=0;
+	if ((in_fp = fopen("inst.dat", "r")) == NULL)
+		return -1;
+	else {
+		char ch[10];
+		while(fscanf(in_fp,"%s",ch)==1){
+			inst[i].str = (char *)malloc(sizeof(ch));
+			strcpy(inst[i].str,ch);
+			fscanf(in_fp,"%s",ch);
+			inst[i].format = atoi(ch);
+			fscanf(in_fp,"%hhx",&inst[i].op);
+			fscanf(in_fp,"%s",ch);
+			inst[i].ops = atoi(ch);
+			fscanf(in_fp,"%s",ch);
+//			printf("%s %hhx %d %d\n", inst[i].str,inst[i].op,inst[i].format,inst[i].ops);
+			i++;
+		}
+	}
+	fclose(in_fp);
+	inst_index=i;
+	return 0;
+}
+
+/* -----------------------------------------------------------------------------------
+* 설명 : 어셈블리 할 소스코드를 읽어오는 함수이다.
+* 매계 : 어셈블리할 소스파일명
+* 반환 : 정상종료 = 0 , 에러 < 0
+* 주의 :
+*
+* -----------------------------------------------------------------------------------
+*/
+
+
+int init_input_file(char *input_file)
+{
+	FILE *in_fp;
+	char buffer[1024];
+	if ((in_fp = fopen("input.txt", "r")) == NULL)
+		return -1;
+	else{
+		while(fgets(buffer,1024,in_fp)){
+			input_data[line_num] = (char*)malloc(sizeof(char)*strlen(buffer)+1);
+			strcpy(input_data[line_num++],buffer);
+		}
+	}
+	fclose(in_fp);
+	return 0;
+}
+
+/* -----------------------------------------------------------------------------------
+* 설명 : 소스 코드를 읽어와 토큰단위로 분석하고 토큰 테이블을 작성하는 함수이다.
+*        패스 1로 부터 호출된다.
+* 매계 : 소스코드의 라인번호
+* 반환 : 정상종료 = 0 , 에러 < 0
+* 주의 : my_assembler 프로그램에서는 라인단위로 토큰 및 오브젝트 관리를 하고 있다.
+* -----------------------------------------------------------------------------------
+*/
+
+int token_parsing(int index)
+{
+	char *token;
+	const char s[2] = "\t";
+	int i;
+	if(input_data[index][0]=='.')return -1;
+	token = strtok(input_data[index],s);
+	// while(token!=NULL){
+		// printf("%s\n",token);
+		// token = strtok(NULL,s);
+	// }
+	// printf("\n");
+	if(input_data[index][0]=='\t'){//no label
+		token_table[token_line].label = (char *)malloc(sizeof(char)*4);
+		strcpy(token_table[token_line].label,"");
+//			printf("%s",token_table[token_line].label);
+	}
+	else{
+		token_table[token_line].label = (char *)malloc(sizeof(char)*strlen(token)+1);
+		strcpy(token_table[token_line].label,token);
+//			printf("%s",token_table[token_line].label);
+		token = strtok(NULL,s);
+	}
+
+
+
+	token_table[token_line].operator_ = (char *)malloc(sizeof(char)*strlen(token)+1);
+	if(token[strlen(token)-1]=='\n'){
+		token[strlen(token)-1] = '\0';
+	}
+	strcpy(token_table[token_line].operator_,token);
+//		printf(".%s\t.",token_table[token_line].operator_);
+	token = strtok(NULL,s);
+
+	if(token==NULL){
+		return 0;
+	}
+
+	token_table[token_line].operand[0] = (char *)malloc(sizeof(char)*strlen(token)+1);
+	if(token[strlen(token)-1]=='\n'){
+		token[strlen(token)-1]='\0';
+	}
+	strcpy(token_table[token_line].operand[0],token);
+//	printf("%s",token_table[token_line].operand[0]);
+//
+//	if(strcmp(token_table[token_line].operator_,"RSUB")!=0
+//			||strcmp(token_table[token_line].operator_,"LTORG")!=0){
+//		token = strtok(NULL,s);
+//	}
+//	if(token==NULL){
+//		token_table[token_line].comment = NULL;
+//			printf("\n");
+//	}
+//	else{
+//		token_table[token_line].comment = (char *)malloc(sizeof(char)*strlen(token)+1);
+//		if(token[strlen(token)-1]=='\n'){
+//			token[strlen(token)-1]='\0';
+//		}
+//		strcpy(token_table[token_line].comment,token);
+//			printf("%s\n",token_table[token_line].comment);
+//	}
+//
+	return 0;
+}
+/* -----------------------------------------------------------------------------------
+* 설명 : 입력 문자열이 기계어 코드인지를 검사하는 함수이다.
+* 매계 : 토큰 단위로 구분된 문자열
+* 반환 : 정상종료 = 기계어 테이블 인덱스, 에러 < 0
+* 주의 :
+*
+* -----------------------------------------------------------------------------------
+*/
+
+int search_opcode(char *str)
+{
+	/* add your code here */
+	int i=0;
+	for(;i<inst_index;++i){
+		if(str[0]=='+'&&strcmp(&str[1],inst[i].str)==0){
+			return i;
+		}
+		else if(strcmp(str,inst[i].str)==0){
+			return i;
+		}
+	}
+	return -1;
+}
+/* -----------------------------------------------------------------------------------
+* 설명 : 입력된 문자열의 이름을 가진 파일에 프로그램의 결과를 저장하는 함수이다.
+* 매계 : 생성할 오브젝트 파일명
+* 반환 : 없음
+* 주의 : 만약 인자로 NULL값이 들어온다면 프로그램의 결과를 표준출력으로 보내어
+*        화면에 출력해준다.
+*
+* -----------------------------------------------------------------------------------
+*/
+
+void make_objectcode(char *file_name)
+{
+	/* add your code here */
+
+}
+
---
+++ sp2/my_assembler.h
@@ -0,0 +1,99 @@
+/*
+* my_assembler 함수를 위한 변수 선언 및 매크로를 담고 있는 헤더 파일이다.
+*
+*/
+#define MAX_INST 256
+#define MAX_LINES 5000
+
+#define MAX_COLUMNS 4
+#define MAX_OPERAND 3
+
+#define SECTOR_MAX 10
+
+/*
+* 기계어 목록 파일로 부터 정보를 받아와 생성하는 기계어 변환 테이블이다.
+* 해당 기계어의 정보를 토큰으로 나눠 기록하고 있다.
+*/
+struct inst_struct {
+	char *str;
+	unsigned char op;
+	int format;
+	int ops;
+};
+typedef struct inst_struct inst_struct;
+inst_struct inst[MAX_INST];
+int inst_index;
+//
+//struct my_opcode{
+//	unsigned char op;
+//	int n;
+//	int i;
+//	int x;
+//	int b;
+//	int p;
+//	int e;
+//	int dist;
+//};
+//struct my_opcode myop;
+/*
+* 어셈블리 할 소스코드를 토큰 단위로 관리하는 테이블이다.
+* 관리 정보는 소스 라인 단위로 관리되어진다.
+*/
+char *input_data[MAX_LINES];
+static int line_num;
+
+int label_num;
+
+struct token_unit {
+	char *label;
+	char *operator_;
+	char *operand[MAX_OPERAND];
+	char *comment;
+	int addr;	//address
+	int length;//use bytes
+	char *opcode;
+};
+
+typedef struct token_unit token;
+token token_table[MAX_LINES];
+static int token_line;
+
+
+
+/*
+* 심볼을 관리하는 구조체이다.
+* 심볼 테이블은 심볼 이름, 심볼의 위치로 구성된다.
+*/
+struct symbol_unit {
+	char symbol[10];
+	int addr;
+};
+
+typedef struct symbol_unit symbol;
+symbol sym_table[SECTOR_MAX][MAX_LINES];
+static int symbol_line[SECTOR_MAX];
+
+struct literal_unit{
+	char * literal;
+	int length;
+};
+
+struct literal_unit liter[SECTOR_MAX][MAX_LINES];
+static int literal_number[SECTOR_MAX];
+
+static int locctr[SECTOR_MAX];
+
+static int sectionIndex;
+char * sections[SECTOR_MAX];
+//--------------
+
+static char *input_file;
+static char *output_file;
+
+int init_my_assembler(void);
+static int assem_pass1(void);
+static int assem_pass2(void);
+int init_inst_file(char *inst_file);
+int init_input_file(char *input_file);
+int search_opcode(char *str);
+void make_objectcode(char *file_name);
