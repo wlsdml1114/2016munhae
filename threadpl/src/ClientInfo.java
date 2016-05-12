ca11f5826b977cfc565593e5374c24dcb6fe81ee
jini <wlsdml1114@gmail.com>
2016-05-12 16:42:25 +0900

    thread program without GUI

---
+++ threadpl/src/ClientInfo.java
@@ -0,0 +1,10 @@
+
+import java.net.Socket;
+
+public class ClientInfo
+{
+    public Socket mSocket = null;
+    public ClientListener mClientListener = null;
+    public ClientSender mClientSender = null;
+}
+ 
