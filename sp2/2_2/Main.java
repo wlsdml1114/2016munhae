83644d294809d1236f03cd0b47a9e02984d93201
jini <wlsdml1114@gmail.com>
2016-05-22 03:15:55 +0900

    sp2_2 update

--- sp2/2_2/Main.java
+++ sp2/2_2/Main.java
@@ -341,8 +341,6 @@ class inst_struct{
 						int second = search_label(ar[1], sectionIndex-1);
 						token_table[token_line].addr = sym_table[sectionIndex-1][first].addr -sym_table[sectionIndex-1][second].addr; 
 					}
-
-
 				}
 				else{
 					return;
@@ -357,183 +355,123 @@ class inst_struct{
 			token_line++;
 		}
 	}
+	
+	public int AXST(char s){
+		if (s=='X')return 1;
+		else if(s=='A')return 0;
+		else if(s=='S')return 4;
+		else return 5;
+	}
 	public void pass2(){
 		int i=0,indexOfInst,section=0;
 		for(;i<token_line;++i){
-			printf("%04X\t%s\t%s",token_table[i].addr,token_table[i].label,token_table[i].operator_);
-			if(strcmp(token_table[i].operator_,"RSUB")==0){
-				printf("\t");
+			System.out.print(String.format("%04X\t%s\t%s",token_table[i].addr,token_table[i].label,token_table[i].operator_));
+			if(token_table[i].operator_.compareTo("RSUB")==0){
+				System.out.print("\t");
 			}
-			else if(token_table[i].operand[0]!=NULL){
-				printf("\t%s",token_table[i].operand[0]);
+			else if(token_table[i].operand[0].length()>0){
+				System.out.print(String.format("\t%s",token_table[i].operand[0]));
 			}
-			if(token_table[i].operand[1]!=NULL){
-				printf(",%s",token_table[i].operand[1]);
+			if(token_table[i].operand[1].length()>0){
+				System.out.print(String.format(",%s",token_table[i].operand[1]));
 			}
-			if( strcmp(token_table[i].operator_, "CSECT") == 0){
+			if( token_table[i].operator_.compareTo("CSECT") == 0){
 				section++;
 			}
-			token_table[i].opcode= (char *)malloc(token_table[i].length*2);
-			char reg[6];
-			if(strcmp(token_table[i].operator_,"CLEAR")==0){
-				sprintf(token_table[i].opcode, "%X", 0xB);
-				sprintf(token_table[i].opcode+1, "%X", 0x4);
-				sprintf(token_table[i].opcode+2, "%X", AXST(token_table[i].operand[0][0]));
-				sprintf(token_table[i].opcode+3, "%X", 0);
+			int[] reg = new int[6];
+			if(token_table[i].operator_.compareTo("CLEAR")==0){
+				token_table[i].opcode="B4"+AXST(token_table[i].operand[0].charAt(0))+"0";
 			}
-
-			else if(strcmp(token_table[i].operator_,"COMPR")==0){
-				sprintf(token_table[i].opcode, "%X", 0xA);
-				sprintf(token_table[i].opcode+1, "%X", 0x0);
-				sprintf(token_table[i].opcode+2, "%X", AXST(token_table[i].operand[0][0]));
-				sprintf(token_table[i].opcode+3, "%X", AXST(token_table[i].operand[0][2]));
+			else if(token_table[i].operator_.compareTo("COMPR")==0){
+				token_table[i].opcode = "A0"+AXST(token_table[i].operand[0].charAt(0))+""+AXST(token_table[i].operand[0].charAt(2));
 			}
-
-			else if(strcmp(token_table[i].operator_,"TIXR")==0){
-				sprintf(token_table[i].opcode, "%X", 0xB);
-				sprintf(token_table[i].opcode+1, "%X", 0x8);
-				sprintf(token_table[i].opcode+2, "%X", AXST(token_table[i].operand[0][0]));
-				sprintf(token_table[i].opcode+3, "%X", 0);
+			else if(token_table[i].operator_.compareTo("TIXR")==0){
+				token_table[i].opcode="B8"+AXST(token_table[i].operand[0].charAt(0))+"0";
 			}
 			else if((indexOfInst = search_opcode(token_table[i].operator_)) > -1){
 				int symnum;
-				if(token_table[i].operand[0][0]=='#'){//nixbpe
+				if(token_table[i].operator_.compareTo("RSUB")==0){
+					reg[0] = 1; reg[1] = 1; reg[2] = 0;
+					reg[3] = 0; reg[4] = 0; reg[5] = 0;
+					token_table[i].opcode+= String.format("4%X%X000",0xC+reg[0]*2+reg[1],reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
+				}
+				else if(token_table[i].operand[0].charAt(0)=='#'){//nixbpe
 					reg[0] = 0; reg[1] = 1; reg[2] = 0;
 					reg[3] = 0; reg[4] = 0; reg[5] = 0;
-
-
-					token_table[i].opcode = (char*)malloc(6);
-					sprintf(token_table[i].opcode,"%X",inst[indexOfInst].op/16);
-					sprintf(token_table[i].opcode+1,"%X",inst[indexOfInst].op%16+reg[0]*2+reg[1]);
-					sprintf(token_table[i].opcode+2,"%X",reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
-					sprintf(token_table[i].opcode+3,"%03X",atoi(&token_table[i].operand[0][1]));
-
-				}else if(token_table[i].operator_[0]=='+'){
+					String opr = token_table[i].operand[0].substring(1, token_table[i].operand[0].length());
+					token_table[i].opcode=String.format("%X%X%X%03X", inst[indexOfInst].op/16,inst[indexOfInst].op%16+reg[0]*2+reg[1],reg[2]*8+reg[3]*4+reg[4]*2+reg[5],Integer.parseInt(opr));
+				}else if(token_table[i].operator_.charAt(0)=='+'){
 					reg[0] = 1; reg[1] = 1; reg[2] = 0;
 					reg[3] = 0; reg[4] = 0; reg[5] = 1;
-					if(token_table[i].operand[0][strlen(token_table[i].operand[0])-1]=='X'){
-						reg[2]=1;
-					}
-					token_table[i].opcode = (char*)malloc(8);
-					sprintf(token_table[i].opcode,"%X",inst[indexOfInst].op/16);
-					sprintf(token_table[i].opcode+1,"%X",inst[indexOfInst].op%16+reg[0]*2+reg[1]);
-					sprintf(token_table[i].opcode+2,"%X",reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
-					sprintf(token_table[i].opcode+3,"00000");
-
-					char *token;
-					const char s[2] = ",";
-					token = strtok(token_table[i].operand[0],s);
-					Modi[section][modi_num[section]].name = (char *)malloc(sizeof(token));
-					strcpy(Modi[section][modi_num[section]].name,token);
+					if(token_table[i].operand[0].charAt(token_table[i].operand[0].length()-1)=='X')reg[2]=1;
+					token_table[i].opcode = String.format("%X%X%X00000", inst[indexOfInst].op/16,inst[indexOfInst].op%16+reg[0]*2+reg[1],reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
+					String [] starr = token_table[i].operand[0].split(",");
+					Modi[section][modi_num[section]].name = starr[0];
 					Modi[section][modi_num[section]].address = token_table[i].addr+1;
 					Modi[section][modi_num[section]].flag = 1;
 					Modi[section][modi_num[section]++].length = 5;
-
-
-//					printf("%s\n%s\n%s\n%s\n%s\n",Modifi[0][0],Modifi[0][1],Modifi[1][0],Modifi[1][1],Modifi[2][1]);
-				}else if(token_table[i].operand[0][0]=='@'){
+				}else if(token_table[i].operand[0].charAt(0)=='@'){
 					reg[0] = 1; reg[1] = 0; reg[2] = 0;
 					reg[3] = 0; reg[4] = 1; reg[5] = 0;
-					token_table[i].opcode = (char*)malloc(6);
-					sprintf(token_table[i].opcode,"%X",inst[indexOfInst].op/16);
-					sprintf(token_table[i].opcode+1,"%X",inst[indexOfInst].op%16+reg[0]*2+reg[1]);
-					sprintf(token_table[i].opcode+2,"%X",reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
-					if ((symnum = search_label(&token_table[i].operand[0][1],section)) > -1){
-//						if ( >= 0) {
-						sprintf(token_table[i].opcode+3,"%03X",(sym_table[section][symnum].addr - token_table[i + 1].addr)%0x1000);
-//						}
-					}
+					String opr = token_table[i].operand[0].substring(1, token_table[i].operand[0].length());
+					symnum = search_label(opr,section);
+					token_table[i].opcode = String.format("%X%X%X%03X",inst[indexOfInst].op/16,inst[indexOfInst].op%16+reg[0]*2+reg[1],reg[2]*8+reg[3]*4+reg[4]*2+reg[5],(sym_table[section][symnum].addr - token_table[i + 1].addr)%4096);
 				}else {
 					reg[0] = 1; reg[1] = 1; reg[2] = 0;
 					reg[3] = 0; reg[4] = 1; reg[5] = 0;
-
-					token_table[i].opcode = (char*)malloc(6);
-					sprintf(token_table[i].opcode,"%X",inst[indexOfInst].op/16);
-					sprintf(token_table[i].opcode+1,"%X",inst[indexOfInst].op%16+reg[0]*2+reg[1]);
-					sprintf(token_table[i].opcode+2,"%X",reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
-					if(token_table[i].operand[0][0]=='='){
-
-						sprintf(token_table[i].opcode+3,"%03X",(liter[section].address-token_table[i+1].addr));
+					token_table[i].opcode = String.format("%X%X%X",inst[indexOfInst].op/16,inst[indexOfInst].op%16+reg[0]*2+reg[1],reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
+					if(token_table[i].operand[0].charAt(0)=='='){
+						token_table[i].opcode+= String.format("%03X", liter[section].address-token_table[i+1].addr);
 					}
 					else{
 						if((symnum = search_label(token_table[i].operand[0],section)) > -1){
 							int res =sym_table[section][symnum].addr - token_table[i + 1].addr;
 							if(res>=0){
-								sprintf(token_table[i].opcode+3,"%03X",res%0x1000);
+								token_table[i].opcode+= String.format("%03X",res%0x1000);
 							}
 							else{
 								res = res&0xFFF;
-								sprintf(token_table[i].opcode+3,"%03X",res%0x1000);
+								token_table[i].opcode+= String.format("%03X",res%0x1000);
 							}
 						}
 					}
-
-
 				}
-
-				if(strcmp(token_table[i].operator_,"RSUB")==0){
-					reg[0] = 1; reg[1] = 1; reg[2] = 0;
-					reg[3] = 0; reg[4] = 0; reg[5] = 0;
-					token_table[i].opcode = (char*)malloc(6);
-					sprintf(token_table[i].opcode,"%X",0x4);
-					sprintf(token_table[i].opcode+1,"%X",0xC+reg[0]*2+reg[1]);
-					sprintf(token_table[i].opcode+2,"%X",reg[2]*8+reg[3]*4+reg[4]*2+reg[5]);
-					sprintf(token_table[i].opcode+3,"000");
-				}
-
-
 			}
-
-
-			else if(strcmp(token_table[i].operator_,"END")==0){
-				printf("\n%04X\t*\t%s\t",token_table[i].addr,liter[section].literal);
-				token_table[i].opcode = (char*)malloc((strlen(liter[section].literal)-4));
+			else if(token_table[i].operator_.compareTo("END")==0){
+				System.out.print(String.format("\n%04X\t*\t%s\t",token_table[i].addr,liter[section].literal));
 				int j=0;
-				for(;j<(strlen(liter[section].literal)-4);++j){
-					sprintf(token_table[i].opcode+j, "%c", liter[section].literal[3+j]);
+				for(;j<(liter[section].literal.length()-4);++j){
+					token_table[i].opcode+=liter[section].literal.charAt(3+j);
 				}
 			}
-
-			else if(strcmp(token_table[i].operator_,"BYTE")==0){
+			else if(token_table[i].operator_.compareTo("BYTE")==0){
 				int j=0;
-				for(;j<(strlen(token_table[i].operand[0])-3);++j){
-					sprintf(token_table[i].opcode+j, "%c", token_table[i].operand[0][2+j]);
+				for(;j<(token_table[i].operand[0].length()-3);++j){
+					token_table[i].opcode+=token_table[i].operand[0].charAt(2+j);
 				}
 			}
-			else if(strcmp(token_table[i].operator_,"WORD")==0){
-				char *token;
-				char buffer[1024];
-				strcpy(buffer,token_table[i].operand[0]);
-				const char s[2] = "-";
-				token = strtok(buffer,s);
-				Modi[section][modi_num[section]].name = (char *)malloc(sizeof(token));
-				strcpy(Modi[section][modi_num[section]].name,token);
+			else if(token_table[i].operator_.compareTo("WORD")==0){
+				String buffer = token_table[i].operand[0];
+				String[] buf = buffer.split("-");
+				Modi[section][modi_num[section]].name=buf[0];
 				Modi[section][modi_num[section]].address = token_table[i].addr;
 				Modi[section][modi_num[section]].flag = 1;
 				Modi[section][modi_num[section]++].length = 6;
-
-				token = strtok(NULL,s);
-				Modi[section][modi_num[section]].name = (char *)malloc(sizeof(token));
-				strcpy(Modi[section][modi_num[section]].name,token);
+				Modi[section][modi_num[section]].name=buf[1];
 				Modi[section][modi_num[section]].address = token_table[i].addr;
 				Modi[section][modi_num[section]].flag = 0;
 				Modi[section][modi_num[section]++].length = 6;
-				token_table[i].opcode = (char *)malloc(6);
-				sprintf(token_table[i].opcode,"000000");//because both value is 0000;
-//				printf("%d%s%d%x\n%d%s%d%x",Modi[section][0].flag,Modi[section][0].name,Modi[section][0].length,Modi[section][0].address,Modi[section][1].flag,Modi[section][1].name,Modi[section][1].length,Modi[section][1].address);
+				token_table[i].opcode = "000000";
 			}
-
-			else if(strcmp(token_table[i].operator_,"LTORG")==0){
-				printf("\n%04X\t*\t\t%s",token_table[i].addr,liter[section].literal);
-				token_table[i].opcode = (char*)malloc((strlen(liter[section].literal)-4)*2);
+			else if(token_table[i].operator_.compareTo("LTORG")==0){
+				System.out.print(String.format("\n%04X\t*\t\t%s",token_table[i].addr,liter[section].literal));
 				int j=0;
-				for(;j<(strlen(liter[section].literal)-4);++j){
-					sprintf(token_table[i].opcode+j*2, "%X", liter[section].literal[3+j]/16);
-					sprintf(token_table[i].opcode+1+j*2, "%X", liter[section].literal[3+j]%16);
+				token_table[i].opcode="";
+				for(;j<(liter[section].literal.length()-4);++j){
+					token_table[i].opcode+= String.format("%X%X", liter[section].literal.charAt(3+j)/16,liter[section].literal.charAt(3+j)%16);
 				}
 			}
-
-			printf("\t%s\n",token_table[i].opcode);
+			System.out.print(String.format("\t%s\n",token_table[i].opcode));
 		}
 		return;
 	}
