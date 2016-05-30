f72d9a4d286e0168b58639b84a73a948cf56beef
jini <wlsdml1114@gmail.com>
2016-05-30 13:26:04 +0900

    10th homework finish

--- 10&backjoon_1339&power_transmission/1339.cpp
+++ 10&backjoon_1339&power_transmission/1339.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <string.h>
 using namespace std;
-// int matrix[10][26];
 int answer[26] = {};
 int jaritsu[8] = {1,10,100,1000,10000,100000,1000000,10000000};
 int main(){
@@ -12,16 +11,14 @@ int main(){
 	while(m--){
 		char ch[9];
 		scanf("%s",ch);
-		// printf("%d",strlen(ch));
 		int j=0;
 		for(int i=strlen(ch)-1;i>=0;--i){
 			answer[ch[j++]-'A'] += jaritsu[i];
-			// printf("%d",ch[j++]-'A');
 		}
 	}
 	int sum=0;
 	int num=9;
-	for(int k=0;k<n;++k){
+	for(int k=0;k<10;++k){
 		int index=0;
 		for(int l=0;l<26;++l){
 			if(answer[index]<answer[l])
